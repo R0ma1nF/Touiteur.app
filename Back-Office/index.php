f7da97b12b11cc3907bea9dbf0388fc28578a166
lucas <lucas.vanneste5@etu.univ-lorraine.fr>
2023-11-10 10:38:05 +0100

    temporaire admin

--- Back-Office/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// index.php
-require 'vendor/autoload.php';
-
-use iutnc\BackOffice\dispatch\Dispatcher;
-session_start();
-$dispatcher = new Dispatcher();
-$dispatcher->run();
--- Back-Office/src/classes/exception/AuthException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace iutnc\BackOffice\exception;
-
-class AuthException extends \Exception {
-    public function __construct($message = "Erreur d'authentification", $code = 0, \Throwable $previous = null) {
-        parent::__construct($message, $code, $previous);
-    }
-}
-?>
